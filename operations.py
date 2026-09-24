e2920366d1716f97caf888112ebd05a807c7374a
mmahirv <mmahirv@hotmail.com>
2026-09-24 22:28:54 +0200

    Created main structure

---
+++ operations.py
@@ -0,0 +1,15 @@
+def sum(list_of_numbers):
+    #taking list and sum all of them
+    return None
+
+def mulp():
+    
+    return None
+
+def update():
+    
+    return None
+
+def delete():
+    
+    return None
