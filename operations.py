f06746a6edf2ecab7080502f4bbefd92c562f98d
mmahirv <mmahirv@hotmail.com>
2026-09-24 22:56:43 +0200

    mulp updated recursive func

--- operations.py
+++ operations.py
@@ -2,9 +2,10 @@ def sum(a,b):
     #taking list and sum all of them
     return a+b
 
-def mulp(a,b):
-    
-    return a*b
+def mulp(b):
+    if(b <= 0):
+        return 1
+    return b * mulp(b-1)
 
 def update():
     
